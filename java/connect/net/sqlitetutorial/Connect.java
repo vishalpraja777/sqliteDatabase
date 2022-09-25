3ec83d93de1043cb701b32529031a8a334d7ed4d
vishalpraja777 <vishalprajapathi7@gmail.com>
2022-09-25 22:15:33 +0530

    Second Commit

--- java/connect/net/sqlitetutorial/Connect.java
+++ java/connect/net/sqlitetutorial/Connect.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 /**
  *
@@ -87,16 +88,73 @@ public class Connect {
         }
     }
 
+    public void selectAll(){
+        String sql = "SELECT id, movies, actor,actress,director,year FROM movies";
+        
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+            
+
+            System.out.println("\nResult:");
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("movies") + "\t" +
+                                   rs.getString("actor") + "\t" +
+                                   rs.getString("actress") + "\t" +
+                                   rs.getString("director") + "\t" +
+                                   rs.getInt("year"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void selectActor(String name)
+    {
+        String sql = "SELECT * FROM movies WHERE actor = ?";
+        
+        try (Connection conn = this.connect();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+            
+            // set the value
+            pstmt.setString(1,name);
+            //
+            ResultSet rs  = pstmt.executeQuery();
+            
+            System.out.println("\nResult:");
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") +  "\t" + 
+                                   rs.getString("movies") + "\t" +
+                                   rs.getString("actor") + "\t" +
+                                   rs.getString("actress") + "\t" +
+                                   rs.getString("director") + "\t" +
+                                   rs.getInt("year"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // connect();
-        createNewDatabase("movies.db"); 
-        createNewTable();
         Connect app = new Connect();
+
+        createNewDatabase("movies.db");
+
+        createNewTable();
+
+        //Insert into tables
         app.insert(1,"Bahubabli","Yash","Srinidhi","Prashanth",2018);
         app.insert(2,"Bahubabli2","Yash","Srinidhi","Prashanth",2022);
         app.insert(3,"Dangal","Aamir","Fatima","Nitesh",2016);
+
+        //Select rows
+        app.selectAll();
+        app.selectActor("Yash");
     }
 }
